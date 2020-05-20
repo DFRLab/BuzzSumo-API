c0b60f67dcafd31da34aaf769a6b137ea936dcf7
estebanpdl <pdlr.estebanfelipe@gmail.com>
2020-05-19 23:28:29 -0500

    Update

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ This repository uses Python code to connect to BuzzSumo's API
 ## Resources
 
 [BuzzSumo's API Reference](https://developers.buzzsumo.com/reference)
+[Epoch & Unix Timestamp Conversion Tools](https://www.epochconverter.com/)
+[Language - List of ISO 639-1 codes](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
+
+#### `R lang`
+[httr - R requests][https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html]
--- api/main.py
+++ api/main.py
@@ -6,10 +6,29 @@ import requests
 import json
 import os
 
+# bar progress
+from tqdm import tqdm
+
+
 # load API KEY
-path = os.path.abspath('../config/api_key.json')
-with open(path) as auth:
+api_key_path = os.path.abspath('../config/api_key.json')
+with open(api_key_path) as auth:
     api_key = json.load(auth)['api_key']
     auth.close()
 
 # load parameters
+params_path = os.path.abspath('./endpoints/articles.json')
+with open(params_path) as file:
+    params = json.load(file)
+    file.close()
+
+# parameters
+params['api_key'] = api_key
+URL = params['url']
+
+# deleting URL key -> This will be used as variable instead.
+del params['url']
+
+# request
+req = requests.request(method='GET', url=URL, params=params)
+results = req.json()
